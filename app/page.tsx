e86c32958dfd699e66556a77e06417a354af96b4
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-01-04 09:00:26 +0900

    Add Redix UI sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import { Flex, Text, Button } from '@radix-ui/themes';
 
 export default function Home() {
-  return <></>;
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let&apos;s go</Button>
+    </Flex>
+  );
 }
