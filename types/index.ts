c3ef9a5b5a8d092b2004f93ef193948f54c80d1e
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-01-26 15:56:33 +0900

    Add DarkMode Type.

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,7 @@
-export * from './radix-ui';
+export * from "./radix-ui";
+
+export type DarkMode = "dark" | "light";
+export const DARK_MODE = {
+  DARK: "dark",
+  LIGHT: "light",
+} as const satisfies { [key: string]: DarkMode };
