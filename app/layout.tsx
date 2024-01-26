263046f292baaa53ee7d6bde8d4ae303a6663e6d
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-01-26 12:49:25 +0900

    import global.css to RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AutoThemeWrapper } from "@/app/ui/AutoThemeWrapper";
+import "./ui/global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
