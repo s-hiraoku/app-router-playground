a9cd0ea205edea3ad47d6f9bf1e8f65995aa56d6
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-01-26 12:04:58 +0900

    Refactor AutoThemeWrapper.

--- app/ui/AutoThemeWrapper/AutoThemeWrapper.tsx
+++ app/ui/AutoThemeWrapper/AutoThemeWrapper.tsx
@@ -1,13 +1,16 @@
 "use client";
+import { PropsWithChildren } from "react";
 import { useMediaQuery } from "@/app/hooks";
 import { Theme } from "@radix-ui/themes";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-type Props = { children: React.ReactNode };
+type Props = {};
 
-export const AutoThemeWrapper: React.FC<Props> = ({ children }) => {
+export const AutoThemeWrapper: React.FC<PropsWithChildren<Props>> = ({
+  children,
+}) => {
   const appearance = useMediaQuery("(prefers-color-scheme: dark)")
     ? "dark"
     : "light";
