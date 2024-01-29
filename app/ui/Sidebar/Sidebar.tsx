9c00e4d93c5a05f364a12a9b65c9d26e9a6a1724
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-01-29 09:16:12 +0900

    Update Sidebar.

--- app/ui/Sidebar/Sidebar.tsx
+++ app/ui/Sidebar/Sidebar.tsx
@@ -1,8 +1,23 @@
 "use client";
-import styles from "./Sidebar.module.css";
+import { ScrollArea } from "@radix-ui/themes";
+import styles from "./Sidebar.module.scss";
+import classNames from "classnames";
 
-type Props = {};
+type Props = { isOpen: boolean };
 
-export const Sidebar: React.FC<Props> = ({}) => {
-  return <div>Sidebar</div>;
+export const Sidebar: React.FC<Props> = ({ isOpen }) => {
+  return (
+    <aside className={classNames(styles.sidebar, { [styles.open]: isOpen })}>
+      <nav>
+        <ScrollArea>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Works</li>
+            <li>Contact</li>
+          </ul>
+        </ScrollArea>
+      </nav>
+    </aside>
+  );
 };
