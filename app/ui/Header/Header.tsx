f68c730e6e97f5b67102fdd5037c27a0eb4e943e
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-01-29 09:14:15 +0900

    Update Header Component.

--- app/ui/Header/Header.tsx
+++ app/ui/Header/Header.tsx
@@ -1,22 +1,23 @@
 "use client";
 import styles from "./Header.module.scss";
-import { useAtom } from "jotai";
-import { isSidebarOpenAtom } from "@/app/store";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Box, Button, Flex } from "@radix-ui/themes";
+import { useCallback } from "react";
 
-type Props = {};
+type Props = { onHamburgerClick: () => void };
 
-export const Header: React.FC<Props> = ({}) => {
-  const [isOpen, toggle] = useAtom(isSidebarOpenAtom);
+export const Header: React.FC<Props> = ({ onHamburgerClick }) => {
+  const handleClick = useCallback(() => {
+    onHamburgerClick();
+  }, [onHamburgerClick]);
 
   return (
-    <header>
+    <header className={styles.header}>
       <Flex align="center" justify="between" p="2">
         <Box>
           <Button
             variant="ghost"
-            onClick={() => toggle()}
+            onClick={handleClick}
             className={styles.hamburgerButton}
           >
             <HamburgerMenuIcon width="18" height="18" />
