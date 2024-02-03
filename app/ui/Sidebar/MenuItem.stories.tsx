cc0489f79872b1ddb1743a39d6c1c494ed379d4c
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-02-03 11:46:18 +0900

    Create template of  Menu Item Componet.

---
+++ app/ui/Sidebar/MenuItem.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryFn } from "@storybook/react";
+import { MenuItem } from "./MenuItem";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export default {
+  title: "Components/MenuItem",
+  component: MenuItem,
+} as Meta<typeof MenuItem>;
+
+const Template: StoryFn<typeof MenuItem> = (args) => <MenuItem {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Menu Item",
+  active: false,
+  disabled: false,
+};
+
+export const WithPrefix = Template.bind({});
+WithPrefix.args = {
+  ...Default.args,
+  prefix: <span>🌟</span>,
+};
+
+export const WithSuffix = Template.bind({});
+WithSuffix.args = {
+  ...Default.args,
+  suffix: <span>🔗</span>,
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  ...Default.args,
+  icon: GitHubLogoIcon,
+};
+
+export const Active = Template.bind({});
+Active.args = {
+  ...Default.args,
+  active: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Default.args,
+  disabled: true,
+};
--- app/ui/Sidebar/MenuItem.tsx
+++ app/ui/Sidebar/MenuItem.tsx
@@ -1,9 +1,30 @@
-type Props = {};
+import { Button } from "@radix-ui/themes";
+import { ReactNode, PropsWithChildren, ComponentType } from "react";
 
-export const MenuItem: React.FC<Props> = () => {
+type Props = {
+  icon?: ComponentType;
+  prefix?: ReactNode;
+  suffix?: ReactNode;
+  active: boolean;
+  disabled: boolean;
+};
+
+export const MenuItem: React.FC<PropsWithChildren<Props>> = ({
+  icon: Icon,
+  children,
+  prefix,
+  suffix,
+  active = false,
+  disabled = false,
+}) => {
   return (
     <li>
-      <a href="#">Home</a>
+      <Button disabled={disabled}>
+        {Icon && <Icon />}
+        {prefix}
+        {children}
+        {suffix}
+      </Button>
     </li>
   );
 };
