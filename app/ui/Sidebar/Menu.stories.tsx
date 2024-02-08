d9147a457fa4f38b8e9675c1923e2d9d11f34902
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-02-08 22:33:16 +0900

    Update Menu.'

--- app/ui/Sidebar/Menu.stories.tsx
+++ app/ui/Sidebar/Menu.stories.tsx
@@ -1,11 +1,75 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryFn } from "@storybook/react";
 import { Menu } from "./Menu";
+import { MenuItem } from "./MenuItem";
+import { MenuGroup } from "./MenuGroup";
+import { action } from "@storybook/addon-actions";
 
 export default {
-  title: "Components/Menu",
   component: Menu,
 } as Meta<typeof Menu>;
 
-export const Default: StoryObj<typeof Menu> = {
-  args: {},
+const Template: StoryFn<typeof Menu> = (args) => <Menu {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  items: [
+    {
+      label: "Home",
+      onClick: action("clicked Home"),
+    },
+    {
+      label: "About",
+      onClick: action("clicked About"),
+    },
+  ],
+};
+
+export const WithGroups = Template.bind({});
+WithGroups.args = {
+  items: [
+    {
+      title: "General",
+      items: [
+        {
+          label: "Dashboard",
+          onClick: action("clicked Dashboard"),
+        },
+        {
+          label: "Settings",
+          onClick: action("clicked Settings"),
+        },
+      ],
+    },
+    {
+      title: "User",
+      items: [
+        {
+          label: "Profile",
+          onClick: action("clicked Profile"),
+        },
+        {
+          label: "Log out",
+          onClick: action("clicked Log out"),
+        },
+      ],
+    },
+  ],
+};
+
+export const WithIconsAndStatus = Template.bind({});
+WithIconsAndStatus.args = {
+  items: [
+    {
+      icon: "🏠",
+      label: "Home",
+      active: true,
+      onClick: action("clicked Home"),
+    },
+    {
+      icon: "ℹ️",
+      label: "About",
+      disabled: true,
+      onClick: action("clicked About"),
+    },
+  ],
 };
--- app/ui/Sidebar/Menu.tsx
+++ app/ui/Sidebar/Menu.tsx
@@ -1,9 +1,40 @@
-type Props = {};
+import { ComponentProps } from "react";
+import { MenuItem } from "./MenuItem";
+import { MenuGroup } from "./MenuGroup";
 
-export const Menu: React.FC<Props> = () => {
+type Props = {
+  items: (ComponentProps<typeof MenuGroup> | ComponentProps<typeof MenuItem>)[];
+};
+
+export const Menu: React.FC<Props> = ({ items }) => {
   return (
-    <menu>
-      <h1>Menu</h1>
-    </menu>
+    <>
+      {items.map((item) => {
+        if (isMenuGroup(item)) {
+          return (
+            <MenuGroup key={item.title} title={item.title} items={item.items} />
+          );
+        }
+
+        return (
+          <MenuItem
+            key={item.label}
+            icon={item.icon}
+            prefix={item.prefix}
+            suffix={item.suffix}
+            active={item.active}
+            disabled={item.disabled}
+            onClick={item.onClick}
+            label={item.label}
+          />
+        );
+      })}
+    </>
   );
+};
+
+const isMenuGroup = (
+  item: ComponentProps<typeof MenuGroup> | ComponentProps<typeof MenuItem>
+): item is ComponentProps<typeof MenuGroup> => {
+  return item.hasOwnProperty("title");
 };
