cab73b91ca06f15b13d654534a92380a6f59f934
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-02-17 08:50:06 +0900

    Update ErrorFallback Component.

--- app/ui/ErrorFallback/ErrorFallback.tsx
+++ app/ui/ErrorFallback/ErrorFallback.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { FallbackProps } from "react-error-boundary";
 import styles from "./ErrorFallback.module.scss";
-import { Heading } from "@radix-ui/themes";
+import { Callout, Heading } from "@radix-ui/themes";
 import { Cross2Icon } from "@radix-ui/react-icons";
 
 type Props = {} & FallbackProps;
@@ -9,11 +9,13 @@ type Props = {} & FallbackProps;
 export const ErrorFallback: React.FC<Props> = ({ error }) => {
   return (
     <div className={styles.container}>
-      <Heading as="h2" color="tomato" className={styles.title}>
+      <Heading as="h2" color="red" className={styles.title}>
         <Cross2Icon width="24" height="24" />
         エラーが発生しました
       </Heading>
-      <pre>{error.message}</pre>
+      <Callout.Root mt="4" color="red">
+        <Callout.Text>{error.message}</Callout.Text>
+      </Callout.Root>
     </div>
   );
 };
