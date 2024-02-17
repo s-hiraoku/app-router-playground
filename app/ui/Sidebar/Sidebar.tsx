da06ab7008a77bcf2e2ff9b62f9c18266ed34fc2
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-02-17 09:39:57 +0900

    Add control area style in sidebar.'

--- app/ui/Sidebar/Sidebar.tsx
+++ app/ui/Sidebar/Sidebar.tsx
@@ -20,7 +20,7 @@ export const Sidebar: React.FC<Props> = ({ items }) => {
   }, []);
   return (
     <aside className={classNames(styles.sidebar, { [styles.open]: isOpen })}>
-      <Flex align="center" justify="end" p="2">
+      <Flex align="center" justify="end" p="2" className={styles.controller}>
         <IconButton
           onClick={handleElasticSidebarClick}
           variant="ghost"
