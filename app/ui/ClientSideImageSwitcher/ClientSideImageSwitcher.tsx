4b2a5f45588ab4b16bcf2b61e7e0e97ead85afb8
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-17 07:59:27 +0900

    Add ClientSideImageSwitcher component.

---
+++ app/ui/ClientSideImageSwitcher/ClientSideImageSwitcher.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import { isDarkModeAtom } from "@/app/store";
+import { useAtomValue } from "jotai";
+
+type Props = {
+  lightSrc: string;
+  darkSrc: string;
+} & Omit<React.ComponentProps<typeof Image>, "src">;
+
+export const ClientSideImageSwitcher: React.FC<Props> = ({
+  lightSrc,
+  darkSrc,
+  ...props
+}) => {
+  const isDarkMode = useAtomValue(isDarkModeAtom);
+  console.log("isDarkMode", isDarkMode);
+  const image = isDarkMode ? darkSrc : lightSrc;
+  return <Image {...props} src={image} />;
+};
---
+++ app/ui/ClientSideImageSwitcher/index.ts
@@ -0,0 +1,1 @@
+export { ClientSideImageSwitcher } from "./ClientSideImageSwitcher";
