87056e803c79117308924d89ad9baab8e74d10ad
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-22 08:52:48 +0900

    Refactor ClientSideImageSwitcher.

--- app/ui/ClientSideImageSwitcher/ClientSideImageSwitcher.tsx
+++ app/ui/ClientSideImageSwitcher/ClientSideImageSwitcher.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { isDarkModeAtom } from "@/app/store";
 import { useAtomValue } from "jotai";
--- auth.ts
+++ auth.ts
@@ -29,7 +29,8 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
+          const passwordsMatch =
+            user.password && (await bcrypt.compare(password, user.password));
           if (passwordsMatch) return user;
         }
 
