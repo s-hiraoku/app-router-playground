2451e6d5a7df0bdb793c35bdc6bfe7d85a0c4171
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-24 10:18:24 +0900

    Add schemas.

---
+++ schemes/index.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const AuthUserScheme = z.object({
+  email: z
+    .string({
+      invalid_type_error: "Please enter an email address.",
+    })
+    .email({ message: "Please enter a valid email address." }),
+  password: z
+    .string({ invalid_type_error: "Please enter a password." })
+    .min(6, { message: "Name must be at least 6 characters." }),
+});
