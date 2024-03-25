8752cdd64f20b3637e1d166368aadad7819e25e1
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-25 09:26:20 +0900

    add credential form.

--- app/ui/LoginForm/CredentialsForm.tsx
+++ app/ui/LoginForm/CredentialsForm.tsx
@@ -4,36 +4,54 @@ import { Box, Flex, TextFieldInput, Text, Button } from "@radix-ui/themes";
 import { useFormState, useFormStatus } from "react-dom";
 import styles from "./CredentialsForm.module.scss";
 import { authenticate } from "./actions";
+import { ErrorMessages } from "../ErrorMessages";
+import { initialState } from "./models";
 
-type Props = {};
-
-export const CredentialsForm: React.FC<Props> = ({}) => {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+export const CredentialsForm: React.FC = () => {
+  const [state, dispatch] = useFormState(authenticate, initialState);
 
   return (
-    <form action={dispatch} className={styles.container}>
+    <form action={dispatch} className={styles.container} noValidate>
       <Flex direction="column" width="100%" gap="3">
         <Box>
-          <Text as="label" size="1">
+          <Text as="label" size="1" htmlFor="email">
             Email
           </Text>
-          <TextFieldInput placeholder="Your email address" />
+          <TextFieldInput
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            aria-describedby="email-error"
+          />
         </Box>
         <Box>
-          <Text as="label" size="1">
+          {state?.errors.email && (
+            <ErrorMessages id="email-error" errors={state.errors.email} />
+          )}
+        </Box>
+        <Box>
+          <Text as="label" size="1" htmlFor="password">
             Password
           </Text>
-          <TextFieldInput width="100%" color="gray" />
+          <TextFieldInput
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Your password"
+            aria-describedby="password-error"
+          />
+        </Box>
+        <Box>
+          {state?.errors.password && (
+            <ErrorMessages id="password-error" errors={state.errors.password} />
+          )}
         </Box>
         <Box mt="1">
           <LoginButton />
         </Box>
         <Box>
-          {errorMessage && (
-            <>
-              <p>{errorMessage}</p>
-            </>
-          )}
+          {state?.message && <ErrorMessages errors={[state.message]} />}
         </Box>
       </Flex>
     </form>
@@ -44,7 +62,12 @@ const LoginButton = () => {
   const { pending } = useFormStatus();
 
   return (
-    <Button variant="soft" className={styles.button} disabled={pending}>
+    <Button
+      variant="soft"
+      type="submit"
+      className={styles.button}
+      disabled={pending}
+    >
       Login
     </Button>
   );
