de41d2c2562cb561a037e19fc6ca62d26ec4921e
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-25 09:26:40 +0900

    add form action.

--- app/ui/LoginForm/actions.ts
+++ app/ui/LoginForm/actions.ts
@@ -1,21 +1,38 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { AuthUserScheme } from "@/schemes";
 import { AuthError } from "next-auth";
+import { State } from "./models";
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData
-) {
+export async function authenticate(prevState: State, formData: FormData) {
+  const validatedFields = AuthUserScheme.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to authenticate.",
+    };
+  }
+
   try {
     await signIn("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
         case "CredentialsSignin":
-          return "Invalid credentials.";
+          return {
+            errors: {},
+            message: "Invalid credentials.",
+          };
         default:
-          return "Something went wrong.";
+          return {
+            errors: {},
+            message: "Something went wrong.",
+          };
       }
     }
     throw error;
