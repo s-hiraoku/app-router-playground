d54c64f401b2751b4fc53a7651baedd8b52cad3c
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-25 09:18:42 +0900

    fix file name typo.

--- utils/inedex.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentType, memo } from "react";
-
-export function withMemo<T>(
-  Component: ComponentType<T>,
-  propsAreEqual?: (prevProps: T, nextProps: T) => boolean
-) {
-  return memo(Component, propsAreEqual);
-}
