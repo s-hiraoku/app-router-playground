a78050b19d19fa56105fccbad220d586fb48eb32
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-03-27 09:54:56 +0900

    Update schema.prisma.

--- auth.ts
+++ auth.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
-import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import bcrypt from "bcryptjs";
 import { User } from "@prisma/client";
--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -4,8 +4,8 @@ import { User } from "@prisma/client";
 
 export async function fetchUser(email: string) {
   try {
-    const user = await sql`SELECT * from USERS where email=${email}`;
-    return user.rows[0] as User;
+    const user = await sql<User>`SELECT * FROM user WHERE email=${email}`;
+    return user.rows[0];
   } catch (error) {
     console.error("Failed to fetch user:", error);
     throw new Error("Failed to fetch user.");
