28b4edcdb49768785c976d6e514607445d2cf97f
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-04-02 08:03:36 +0900

    Add sign up action

--- app/ui/AuthForm/actions.ts
+++ app/ui/AuthForm/actions.ts
@@ -1,19 +1,22 @@
 "use server";
 
 import { signIn } from "@/auth";
-import { AuthUserScheme } from "@/schemes";
+import { CredentialsUserScheme } from "@/schemes";
 import { AuthError } from "next-auth";
 import { State } from "./models";
+import { sql } from "@vercel/postgres";
+import { User } from "@prisma/client";
+import { PostgresErrorCodes } from "@/types";
 
 export async function authenticate(prevState: State, formData: FormData) {
-  const validatedFields = AuthUserScheme.safeParse({
+  const result = CredentialsUserScheme.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
   });
 
-  if (!validatedFields.success) {
+  if (!result.success) {
     return {
-      errors: validatedFields.error.flatten().fieldErrors,
+      errors: result.error.flatten().fieldErrors,
       message: "Missing Fields. Failed to authenticate.",
     };
   }
@@ -38,3 +41,40 @@ export async function authenticate(prevState: State, formData: FormData) {
     throw error;
   }
 }
+
+export const signUp = async (prevState: State, formData: FormData) => {
+  const result = CredentialsUserScheme.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Sign up.",
+    };
+  }
+
+  const { email, password } = result.data;
+
+  try {
+    await sql<User>`INSERT INTO users (email, password) VALUES (${email}, ${password})`;
+  } catch (error) {
+    if (error instanceof Error && "code" in error) {
+      switch (error.code) {
+        case PostgresErrorCodes.UNIQUE_VIOLATION:
+          return {
+            errors: {},
+            message: "Email is already exists.",
+          };
+        default:
+          console.error("Sign up error:", error);
+          return {
+            errors: {},
+            message: "Failed to sign up due to an unexpected error.",
+          };
+      }
+    }
+    throw error;
+  }
+};
