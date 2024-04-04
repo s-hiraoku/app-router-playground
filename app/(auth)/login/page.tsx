2910dc09dff78bd220cda44491d89a713cb412a0
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-04-04 09:19:47 +0900

    Apply route groups.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import { AuthForm } from "@/app/ui/AuthForm";
 import styles from "./page.module.scss";
-import { GuestLayout } from "../ui/GuestLayout";
+import { GuestLayout } from "@/app/ui/GuestLayout";
 
 export default function LoginPage() {
   return (
