b306124cec9dbef5f96902a892e5c3240db16cf0
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-04-11 09:41:57 +0900

    update auth.ts.

--- auth.ts
+++ auth.ts
@@ -1,20 +1,9 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { sql } from "@vercel/postgres";
 import bcrypt from "bcryptjs";
-import { User } from "@prisma/client";
 import { CredentialsUserScheme } from "./schemes";
-
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
-  }
-}
+import { getUserByEmail } from "@/db/user";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -25,7 +14,7 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
+          const user = await getUserByEmail(email);
           if (!user) return null;
           const passwordsMatch =
             user.password && (await bcrypt.compare(password, user.password));
