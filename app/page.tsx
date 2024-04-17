3417f216513768a7d4c22b920942cab170118e85
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-04-17 09:17:00 +0900

    set user info.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { auth } from "@/auth";
 import { Header } from "@/app/ui/Header";
 
 export default async function Page() {
-  // const session = await auth();
-
   return (
     <div>
       <Header user={undefined} />
