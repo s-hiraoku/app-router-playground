bee24373930ef0c3f5a068b85c8dd4923130999b
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-05-10 09:02:43 +0900

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,44 +1,4 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import { CredentialsUserScheme } from "./schemes";
-import { getUserByEmail } from "@/db/user";
-import GitHubProvider from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  ...authConfig,
-  providers: [
-    CredentialsProvider({
-      async authorize(credentials) {
-        const parsedCredentials = CredentialsUserScheme.safeParse(credentials);
-
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUserByEmail(email);
-          if (!user) return null;
-          const passwordsMatch =
-            user.password && (await bcrypt.compare(password, user.password));
-          if (passwordsMatch) return user;
-        }
-
-        console.log("Invalid credentials");
-        return null;
-      },
-    }),
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
-});
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
