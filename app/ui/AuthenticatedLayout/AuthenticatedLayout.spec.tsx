d233c046d545fc2d8804907a8aea7c0a09c11a47
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-05-13 09:12:32 +0900

    Delete unnecesarry component.

--- app/ui/AuthenticatedLayout/AuthenticatedLayout.spec.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from "@testing-library/react";
-import { AuthenticatedLayout } from "./AuthenticatedLayout";
-
-test("Enjoy Testing!", () => {
-  render(<AuthenticatedLayout />);
-});
--- app/ui/AuthenticatedLayout/AuthenticatedLayout.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Meta, StoryObj } from "@storybook/react";
-import { AuthenticatedLayout } from "./AuthenticatedLayout";
-
-export default {
-  component: AuthenticatedLayout,
-} as Meta<typeof AuthenticatedLayout>;
-
-export const Default: StoryObj<typeof AuthenticatedLayout> = {};
