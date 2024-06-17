e170ccf5260899a473bdd7d5790e8dd5c0a60769
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-06-17 09:18:45 +0900

    Add error page template.

---
+++ app/(auth)/error/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.scss";
+
+export default function ErrorPage(): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <h1>Error</h1>
+    </main>
+  );
+}
