075ac454f70cce4d04873dd2571c79b443d3c77b
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-06-17 09:13:03 +0900

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,5 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 // NodeJSのグローバル名前空間を拡張
 declare global {
+  // https://stackoverflow.com/questions/69850598/how-to-resolve-this-typescript-error-on-global-node-js-object/69851359#69851359
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
